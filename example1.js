97ae83d41c08af3521404fa2c5538529e7f8dbd3
HNandan30 <124830258+HNandan30@users.noreply.github.com>
2023-03-12 18:14:37 +0000

    Add files via upload

---
+++ example1.js
@@ -0,0 +1,16 @@
+
+<html>
+<head>
+<title>Page Title</title>
+<style>
+width: 400px;
+height: 400px;
+margine: 20px;
+background: red;
+</style>
+</head>
+<body>
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>   
+</body>
+</html>
